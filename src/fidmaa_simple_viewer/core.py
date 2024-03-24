dcb8ba51c63b99095e17e08b7a8d6d2d74a2d6d0
Michał Pasternak <michal.dtz@gmail.com>
2024-03-24 06:45:54 +0100

    Functions

--- src/fidmaa_simple_viewer/core.py
+++ src/fidmaa_simple_viewer/core.py
@@ -4,6 +4,8 @@ import click
 import numpy as np
 from PIL.Image import Image
 
+from portrait_analyser.ios import load_image
+
 
 def depth_map_to_point_cloud(depth_map):
     # This function was written by ChatGPT 3, with some minor corrections
@@ -18,23 +20,27 @@ def depth_map_to_point_cloud(depth_map):
 
     return np.array(points)
 
-def visualise_fidmaa_image(image, depthmap):
-    import cv2
-    import pyvista
 
-    depthmap = np.array(depthmap.convert("L"))
-    res = cv2.flip(depthmap, 0)
+def Image_to_OpenCV(image: Image):
+    return np.array(image)
+
+
+def FIDMAA_to_pyvista_surface(image, depthmap):
+    import pyvista, cv2
+
+    depthmap = Image_to_OpenCV(depthmap.convert("L"))
+    depthmap = cv2.flip(depthmap, 0)
 
     # Load an image to use as a texture
-    colors = np.array(image) # cv2.cvtColor(np.array(image)) # , cv2.COLOR_RGB2BGR)
-    colors = cv2.resize(colors, (640, 480))
+    colors = np.array(image)
+    # colors = cv2.resize(colors, (640, 480))
 
     # Convert depth map to point cloud
-    point_cloud = depth_map_to_point_cloud(res)
+    point_cloud = depth_map_to_point_cloud(depthmap)
 
     pdata = pyvista.PolyData(point_cloud, force_float=False)
     # Compute the surface mesh from the point cloud using the Delaunay triangulation
-    surf = pdata.delaunay_2d(progress_bar=True)
+    surface = pdata.delaunay_2d(progress_bar=True)
 
     # Create a PyVista image object from the RGB image data
     image_pv = pyvista.pyvista_ndarray(colors)
@@ -43,21 +49,32 @@ def visualise_fidmaa_image(image, depthmap):
     texture = pyvista.Texture(image_pv)
 
     # Map the texture onto the PolyData object
-    surf.texture_map_to_plane(inplace=True, use_bounds=True)
+    surface.texture_map_to_plane(inplace=True, use_bounds=True)
 
+    return surface, texture
+
+
+def pyvista_show(surface, texture):
     # Plot the PolyData object with the texture
-    plotter = pyvista.Plotter(line_smoothing=True,)
-    plotter.add_mesh(surf, texture=texture)
+    import pyvista
+    plotter = pyvista.Plotter(line_smoothing=True, )
+    plotter.add_mesh(surface, texture=texture)
     plotter.add_text("FIDMAA (C) 2022-2024 Michal Pasternak & collaborators ")
     plotter.show()
+
+
+def visualise_fidmaa_image(filename):
+    image, depth_map = load_image(filename)
+    surface, texture = FIDMAA_to_pyvista_surface(image, depth_map)
+    pyvista_show(surface, texture)
 
 
 @click.command()
 @click.argument('input')
 def display_fidmaa_image(input):
     from portrait_analyser.ios import load_image
-    image, depth_map = load_image(input)
-    visualise_fidmaa_image(image, depth_map)
+    visualise_fidmaa_image(input)
+
 
 if __name__ == '__main__':
     display_fidmaa_image()
